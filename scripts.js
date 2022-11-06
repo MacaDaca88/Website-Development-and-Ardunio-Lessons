c69be67ce4a0bb839f9b5a495582d103237c600c
Michael Duncan <114211123+MacaDaca88@users.noreply.github.com>
2022-11-07 02:08:14 +1300

    updates

--- scripts.js
+++ scripts.js
@@ -1,10 +1,31 @@
-/* Place your JavaScript in this file */
 
-/*----------wtf do i do here languange makes no senxe x= y 5* 15  75--------------*/
 function myFunction(imgs) {
     var expandImg = document.getElementById("expandedImg");
     var imgText = document.getElementById("imgtext");
     expandImg.src = imgs.src;
     imgText.innerHTML = imgs.alt;
     expandImg.parentElement.style.display = "block";
-  }
+}
+// When the user clicks on div, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
