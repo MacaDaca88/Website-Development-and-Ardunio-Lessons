776f5d1577ae6e90a68098a0a8b2a26a07a32468
Michael Duncan <114211123+MacaDaca88@users.noreply.github.com>
2022-11-06 23:43:45 +1300

    updates

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 /* Place your JavaScript in this file */
 
 /*----------wtf do i do here languange makes no senxe x= y 5* 15  75--------------*/
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
